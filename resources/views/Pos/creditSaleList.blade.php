34f84ed991a772624ca957155dc995d607f15a36
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-11-08 22:15:12 +0630

    update

--- resources/views/Pos/creditSaleList.blade.php
+++ resources/views/Pos/creditSaleList.blade.php
@@ -50,7 +50,6 @@
                 @forelse ($CashSaleData as $item)
                     <tr
                         class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-
                         <th scope="row"
                             class="px-6 text py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ $item->cus_name }}
